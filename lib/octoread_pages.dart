ad52466ad3d1ad5934a6baa44644b260d0b66114
Anar K. Jafarov <anar.k.jafarov@gmail.com>
2020-05-08 03:47:15 +0300

    dependency update

--- lib/octoread_pages.dart
+++ lib/octoread_pages.dart
@@ -1,11 +1,10 @@
-/// octoread_pages
-///
-/// A Aqueduct web server.
 library octoread_pages;
 
 export 'dart:async';
 export 'dart:io';
 
 export 'package:aqueduct/aqueduct.dart';
+
+export 'package:octoread_engine/octoread_engine.dart';
 
 export 'channel.dart';
